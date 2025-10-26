9d1dd72bef1cb2fce73dd69ec4aa28746965d7ef
Makar0w <151269006+Makar0w@users.noreply.github.com>
2025-10-26 14:36:57 -0700

    Major Update
    Arguments have been added "-s" to determine where the 
    static is .
    
    "-p" to dertermine what port to use to run the server .
    
    PS : Don't use port 0 for god sake !

--- serverweb.py
+++ serverweb.py
@@ -4,7 +4,8 @@ import socketserver
 import os
 import time
 import threading
-import ssl
+import argparse
+from colorama import Fore, Back, Style
 principal_title = '''
   |\'/-..--.
  / _ _   ,  ;
@@ -13,17 +14,22 @@ principal_title = '''
  )
 '''
 print(principal_title)
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", help="Where the website template is located", default="static")
+parser.add_argument("-p", type=int, help="Port That The Server will run on" , default="443")
+args = parser.parse_args()
 logging.basicConfig(filename='Log/serverweb.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 def thread_function(name):
     logging.info("Thread %s: starting", name)
     time.sleep(1)
     logging.info("Thread %s: finishing", name)
-
-STATIC_DIR = "static"
-PORT = 443
-
+STATIC_DIR = args.s
+PORT = args.p
+if PORT < 1024 :
+    print(Back.RED + "WARNING : The server is running on a port under 1024 windows may stop the server from running",Back.BLACK +"")
+if PORT == 0:
+    print(Back.RED+"Port invalid !",Back.BLACK +"")
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('X-Frame-Options', 'DENY') 
@@ -47,6 +53,7 @@ def main():
             print("\nServer stopped.")
             logger.error("I whant to say that this server is not secured line 24 , 23 there is only basic protection !")
             logger.critical("\nCRITICAL: Server stopped by Admin Input. This is for maintenance, upgrade, or due to an error.")
+        
 
 if __name__ == "__main__":
     main()
